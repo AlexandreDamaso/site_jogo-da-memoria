a82fb8ef68b23815b30b1d32d84a23e47ac9407e
Alexandre Damaso <alexandredamasocosta@gmail.com>
2024-11-19 10:03:19 -0300

    Criando os cards dinamicamente

---
+++ src/js/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+   "🦁",
+   "🦁",
+   "🐷",
+   "🐷",
+   "🦝",
+   "🦝",
+   "🐼",
+   "🐼",
+   "🐔",
+   "🐔",
+   "🐸",
+   "🐸",
+   "🐵",
+   "🐵",
+   "🐮",
+   "🐮"
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1 ));
+
+for (let i = 0; i < emojis.length; i++) {
+   let box = document.createElement("div");
+   box.className = "item";
+   box.innerHTML = shuffleEmojis[i];
+   document.querySelector(".game").appendChild(box);
+}
